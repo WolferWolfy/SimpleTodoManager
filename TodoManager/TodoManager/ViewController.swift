12a1fac89e7a8b5de8059ca72915069a96478d33
Marton Imre Farkas <marton_imre_farkas@epam.com>
2015-06-15 16:27:42 +0200

    saving, retrieving managed object [todos].

--- TodoManager/TodoManager/ViewController.swift
+++ TodoManager/TodoManager/ViewController.swift
@@ -7,17 +7,46 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names = [String]()
+    var todos = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "\"The List\""
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName:"Todo")
+        
+        //3
+        
+        var fetchedResults : [NSManagedObject]?
+        do {
+            fetchedResults = try managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
+        }
+        catch {
+            print("Could not fetch \(error)")
+        }
+        
+        if let results = fetchedResults {
+            todos  = results
+        }
     }
 
 
@@ -26,12 +55,10 @@ class ViewController: UIViewController, UITableViewDataSource{
         let alert = UIAlertController(title: "New name",  message: "Add a new name", preferredStyle: .Alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .Default) { (action: UIAlertAction!) -> Void in
-            
-            let textField = alert.textFields![0] as UITextField!
-            //TODO check for empty field
-            self.names.append(textField.text!)
-            
-            self.tableView.reloadData()
+                
+                let textField = alert.textFields![0] as UITextField!
+                self.saveTodo(textField.text)
+                self.tableView.reloadData()
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action: UIAlertAction!) -> Void in
@@ -47,16 +74,45 @@ class ViewController: UIViewController, UITableViewDataSource{
         presentViewController(alert, animated: true, completion: nil)
     }
     
+    
+    
+    func saveTodo(title: String?) {
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Todo", inManagedObjectContext: managedContext)
+        
+        let todoItem = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext)
+        
+        //3
+        todoItem.setValue(title, forKey: "title")
+        
+        //4
+        do {
+            try managedContext.save()
+        }
+        catch {
+            print("Could not save \(error)")
+        }
+        //5
+        todos.append(todoItem)
+    }
+    
+    
     // MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return names.count
+            return todos.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
         
-        cell.textLabel!.text = names[indexPath.row]
+        let todoItem = todos[indexPath.row]
+        cell.textLabel!.text = todoItem.valueForKey("title") as? String
 
         return cell
     }
