f2449618471fab949782578c83d9cda5ba67497f
Marton Imre Farkas <marton_imre_farkas@epam.com>
2015-06-15 13:40:04 +0200

    First commit. project created.

---
+++ TodoManager/TodoManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoManager
+//
+//  Created by Farkas Marton Imre on 15/06/15.
+//  Copyright © 2015 Farkas Marton Imre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
